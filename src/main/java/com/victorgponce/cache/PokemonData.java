e257477b4f382bb7be4e224850e6107c0ae1ef77
ponchisao326_ <ponchisao326@hotmail.com>
2026-01-14 23:04:35 +0100

    Refactor OnPokemonCaptured to streamline capture event handling and add PokemonData for tracking captured Pokemon

---
+++ src/main/java/com/victorgponce/cache/PokemonData.java
@@ -0,0 +1,11 @@
+package com.victorgponce.cache;
+
+import com.victorgponce.data_objects.PokemonCaught;
+
+import java.util.ArrayList;
+
+public class PokemonData {
+
+    public static ArrayList<PokemonCaught> pokemonCaughtArrayList = new ArrayList<>();
+
+}
--- src/main/java/com/victorgponce/events/OnPokemonCaptured.java
+++ src/main/java/com/victorgponce/events/OnPokemonCaptured.java
@@ -1,49 +1,47 @@
 package com.victorgponce.events;
 
-import com.cobblemon.mod.common.Cobblemon;
 import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent;
-import com.cobblemon.mod.common.api.pokedex.PokedexManager;
 import com.cobblemon.mod.common.api.pokemon.stats.Stats;
 import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.victorgponce.data_objects.Ivs;
+import com.victorgponce.data_objects.PokemonCaught;
 import com.victorgponce.utils.BigDataUtils;
-import net.minecraft.util.Identifier;
 
-import static com.mojang.text2speech.Narrator.LOGGER;
+import static com.victorgponce.cache.PokemonData.pokemonCaughtArrayList;
 
 public class OnPokemonCaptured {
 
     public static void onCaptureEvent(PokemonCapturedEvent event) {
-        Pokemon pokemon = event.getPokemon();
+        Pokemon pokemonCaptured = event.getPokemon();
 
-        int level = pokemon.getLevel();
-        String nature = pokemon.getNature().getName().getPath();
-        String ability = pokemon.getAbility().getName();
-        boolean shiny = pokemon.getShiny();
+        int level = pokemonCaptured.getLevel();
+        String nature = pokemonCaptured.getNature().getName().getPath();
+        String ability = pokemonCaptured.getAbility().getName();
+        boolean shiny = pokemonCaptured.getShiny();
 
         Ivs ivs = new Ivs(
-                pokemon.getIvs().get(Stats.HP),
-                pokemon.getIvs().get(Stats.ATTACK),
-                pokemon.getIvs().get(Stats.DEFENCE),
-                pokemon.getIvs().get(Stats.SPECIAL_ATTACK),
-                pokemon.getIvs().get(Stats.SPECIAL_DEFENCE),
-                pokemon.getIvs().get(Stats.SPEED)
+                pokemonCaptured.getIvs().get(Stats.HP),
+                pokemonCaptured.getIvs().get(Stats.ATTACK),
+                pokemonCaptured.getIvs().get(Stats.DEFENCE),
+                pokemonCaptured.getIvs().get(Stats.SPECIAL_ATTACK),
+                pokemonCaptured.getIvs().get(Stats.SPECIAL_DEFENCE),
+                pokemonCaptured.getIvs().get(Stats.SPEED)
         );
 
-        String ball_used = pokemon.getCaughtBall().getName().getPath();
+        String ballUsed = pokemonCaptured.getCaughtBall().getName().getPath();
 
         float dexPercentage = BigDataUtils.getNationalDexPercentage(event.getPlayer());
 
-        LOGGER.info("BigData Log: Jugador {} capturó {}, lvl: {}, nature: {}, ability: {}, shiny: {}, ivs: {}, ball: {}, dexPercentage: {}%",
-                event.getPlayer().getName().getString(),
-                pokemon.getSpecies().getName(),
+        PokemonCaught pokemonCaught = new PokemonCaught(pokemonCaptured.getSpecies().getName(),
                 level,
                 nature,
                 ability,
                 shiny,
                 ivs,
-                ball_used,
+                ballUsed,
                 dexPercentage);
+
+        pokemonCaughtArrayList.add(pokemonCaught);
     }
 
 }
