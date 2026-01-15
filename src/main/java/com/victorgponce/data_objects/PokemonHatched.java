d57c9c2d62fe78cd82f1c3f1c559093cc0883285
ponchisao326_ <ponchisao326@hotmail.com>
2026-01-15 01:46:55 +0100

    Add OnEggHatched event handler and PokemonHatched data object for tracking hatched Pokémon

---
+++ src/main/java/com/victorgponce/data_objects/PokemonHatched.java
@@ -0,0 +1,24 @@
+package com.victorgponce.data_objects;
+
+import com.google.gson.Gson;
+
+import java.util.UUID;
+
+public record PokemonHatched(
+        UUID pokemonUuid,
+        String species,
+        boolean shiny,
+        Ivs ivs,
+        String ability,
+        String nature,
+        String ballInherited, // Collection data
+        String playerUuid,
+        String biome,
+        long timestamp
+) {
+    private static final Gson gson = new Gson();
+
+    public String toJson() {
+        return gson.toJson(this);
+    }
+}
