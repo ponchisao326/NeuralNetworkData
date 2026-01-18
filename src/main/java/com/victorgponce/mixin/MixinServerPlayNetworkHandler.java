343c3cf6feed5c121b55e3f443f69a9bb415dc19
ponchisao326_ <ponchisao326@hotmail.com>
2026-01-18 20:11:13 +0100

    Add command filtering and logging in MixinServerPlayNetworkHandler

--- src/main/java/com/victorgponce/mixin/MixinServerPlayNetworkHandler.java
+++ src/main/java/com/victorgponce/mixin/MixinServerPlayNetworkHandler.java
@@ -4,6 +4,8 @@ import com.victorgponce.service.TelemetryFacade;
 import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,10 +17,30 @@ public class MixinServerPlayNetworkHandler {
 
     @Shadow public ServerPlayerEntity player;
 
+    @Shadow
+    @Final
+    private static Logger LOGGER;
+
     @Inject(method = "onCommandExecution", at = @At("HEAD"))
     private void onCommandExecution(CommandExecutionC2SPacket packet, CallbackInfo ci) {
         // Extract command directly from the network packet
         String command = packet.command();
+        LOGGER.info("Info command from NeuralNetworkData: {}", command);
+
+        // Ignore view data command
+        if (command.startsWith("cobbleneuraldata")) return;
+
+        // Ignore Worldedit
+        if (command.startsWith("/") ||
+            command.startsWith("wand") ||
+            command.startsWith("pos1") ||
+            command.startsWith("pos2")) return;
+
+        // Ignore massive construction commands (Admin Staff)
+        if (command.startsWith("fill") || command.startsWith("setblock") || command.startsWith("clone")) return;
+
+        // Ignore empty commands or too shorts (Typos)
+        if (command.length() < 2) return;
 
         // Send to Facade
         TelemetryFacade.getInstance().processCommand(
